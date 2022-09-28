e38dc3ff44f573796c99afc3ef4b5c523090305c
mczimmermann <michellezimmermann@ucsb.edu>
2022-09-28 10:52:50 -0700

    naming + linting errors

--- tools/experiments/idle_ext_pmt.py
+++ tools/experiments/idle_ext_pmt.py
@@ -4,10 +4,10 @@ import time as _t
 from artiq.experiment import *
 from jax import InfiniteLoop, SinaraEnvironment
 
-__all__ = ["IDLE"]
+__all__ = ["IDLE_EXT_PMT"]
 
 
-class IDLE(InfiniteLoop, SinaraEnvironment):
+class IDLE_EXT_PMT(InfiniteLoop, SinaraEnvironment):
     """Base class for background experiment to read PMT and set TTL/DDS parameters from an Arduino.
 
     Inherit this class in the experiment repository and define the class variables:
@@ -17,11 +17,17 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
 
     This experiment assumes that the device has at least one AD9910 DDS and at least one TTL board.
     """
+
     DIFFERENTIAL_TRIGGER = None
     REPUMP_AOM_CHANNELS = None
     kernel_invariants = {
-        "DIFFERENTIAL_TRIGGER", "REPUMP_AOM_CHANNELS", "PMT_EDGECOUNTER", "repump_aoms",
-        "pmt_counter", "ad9910s", "ttl_outs"
+        "DIFFERENTIAL_TRIGGER",
+        "REPUMP_AOM_CHANNELS",
+        "PMT_EDGECOUNTER",
+        "repump_aoms",
+        "pmt_counter",
+        "ad9910s",
+        "ttl_outs",
     }
 
     def build(self):
@@ -110,7 +116,7 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
             self._gate_func(trigger_cycle_mu)
             total_trigger_wait_time_mu += trigger_cycle_mu
 
-            while (total_trigger_wait_time_mu < max_wait_time_mu - trigger_cycle_mu):
+            while total_trigger_wait_time_mu < max_wait_time_mu - trigger_cycle_mu:
                 # wait for a trigger in the cycle
                 gate_end_time_mu = self._gate_func(trigger_cycle_mu)
                 total_trigger_wait_time_mu += trigger_cycle_mu
@@ -124,7 +130,9 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
 
             # takes care of the last section of time
             if trigger_time < 0:
-                gate_end_time_mu = self._gate_func(max_wait_time_mu - total_trigger_wait_time_mu)
+                gate_end_time_mu = self._gate_func(
+                    max_wait_time_mu - total_trigger_wait_time_mu
+                )
                 trigger_time = self.differential_trigger.timestamp_mu(gate_end_time_mu)
 
             if trigger_time > 0:  # if trigger is detected at some point
@@ -164,7 +172,7 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
                 determine whether repump AOMs should be turned on in the differential mode.
         """
         dds_changes = self.cxn.artiq.get_dds_change_queues()
-        to_kernel = [(-1, -1, "placeholder", 0.)]
+        to_kernel = [(-1, -1, "placeholder", 0.0)]
         for kk in dds_changes:
             index = self.devices.ad9910s.index(kk[0])
             try:
@@ -185,14 +193,16 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
             TTL changes. The first element is always a placeholder.
         """
         ttl_changes = self.cxn.artiq.get_ttl_change_queues()
-        to_kernel = [(-1, 0.)]
+        to_kernel = [(-1, 0.0)]
         for kk in ttl_changes:
             index = self.devices.ttl_outs.index(kk[0])
             to_kernel.append((index, kk[1]))
         return to_kernel
 
     @kernel(flags={"fast-math"})
-    def update_dds(self, index: TInt32, index_repump: TInt32, attribute: TStr, value: TFloat):
+    def update_dds(
+        self, index: TInt32, index_repump: TInt32, attribute: TStr, value: TFloat
+    ):
         """Sets DDS value."""
         device = self.ad9910s[index]
         if attribute == "frequency":
@@ -220,7 +230,7 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
             device.set_att(value)
             self.core.break_realtime()
         elif attribute == "state":
-            is_on = value > 0.
+            is_on = value > 0.0
             if is_on:
                 device.sw.on()
             else:
@@ -233,7 +243,7 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
     def update_ttl(self, index: TInt32, value: TFloat):
         """Sets TTL value."""
         device = self.ttl_outs[index]
-        if value > 0.:
+        if value > 0.0:
             device.on()
         else:
             device.off()
@@ -245,7 +255,7 @@ class IDLE(InfiniteLoop, SinaraEnvironment):
             # if the repump AOM is off, don't turn on and off the AOM.
             # the repump AOM stays off for both differential high and low counting periods.
 
-            if self.repump_aom_states[kk] > 0.:
+            if self.repump_aom_states[kk] > 0.0:
                 if turn_off:
                     self.repump_aoms[kk].sw.off()
                 else:
