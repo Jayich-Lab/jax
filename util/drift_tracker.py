afa75c1fc51d91470219119a06dadef0ae5a24fb
Mingyu Fan <mingyufan212@gmail.com>
2022-10-24 17:37:24 -0700

    fixes bug in `sync_time` in the drift tracker (#63)
    * temporary workaround
    
    * updates the drift tracker code
    
    * pep8 fix
    
    * override run function
    
    * Solves sync_time issue
    
    We can now correctly sync our experiment time to epoch time during an experiment
    
    Co-authored-by: royaready <63123871+Spencer-Kofford@users.noreply.github.com>
    Co-authored-by: mczimmermann <michellezimmermann@ucsb.edu>
    Co-authored-by: royaready <77765489+Samuel-Gebe@users.noreply.github.com>

--- util/drift_tracker.py
+++ util/drift_tracker.py
@@ -61,19 +61,28 @@ class DriftTracker:
         self._center_drift_rate_mu = 0.
         self.uB_over_h = _c.physical_constants["Bohr magneton"][0] / _c.h
 
+    @rpc
+    def get_epoch_time(self) -> TFloat:
+        """Returns the current epoch time in seconds."""
+        return _t.time()
+
     @kernel(flags={"fast-math"})
-    def sync_time(self, time_now):
+    def sync_time(self, mu: TFloat = 1e-9):
         """Syncs wall clock time with core device time.
 
-        Must be called if self.get_frequency_kernel or self.get_Zeeman_frequency_kernel are used.
+        This function sets the drift rate and calibration time in machine units.
+        It needs to be called once before `get_frequency_kernel` or `get_Zeeman_frequency_kernel`
+        is used. This function contains a remote procedure call so it may be slow.
 
         Args:
             time_now: float, epoch time now.
+            mu: float, machine unit of time in s. Default 1e-9 (1 ns).
         """
+        time_now = self.get_epoch_time()
         time_after_calibration = time_now - self.last_calibration
-        self._last_calibration_mu = now_mu() - self.core.seconds_to_mu(time_after_calibration)
+        self._last_calibration_mu = now_mu() - _np.int64(time_after_calibration / mu)
         # time is converted to machine units, frequency is still in Hz.
-        self._center_drift_rate_mu = self.center_drift_rate * self.core.mu_to_seconds(1)
+        self._center_drift_rate_mu = self.center_drift_rate * mu
 
     @host_only
     def get_frequency_host(self, detuning):
