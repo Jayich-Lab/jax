d42d70270b78d536eed7d7c5290bb212b8df54a3
occheung <ocheung@ucsb.edu>
2023-01-06 13:43:53 -0800

    ad9910_ram: format docstring

--- base/experiments/ad9910_ram.py
+++ base/experiments/ad9910_ram.py
@@ -28,87 +28,99 @@ class RAMType(Enum):
 
 
 class RAMProfile:
-    """RAM profile is an operation mode that feeds RAM data into the DDS as
-        DDS parameter(s) (frequency/phase/amplitude). At an user-defined time
-        interval, the DDS fetches a new RAM data and updates the selected
-        parameter. See the arguments of the constructor (__init__(..)) to tune
-        the interval, DDS parameter and data selection of the RAM playback.
+    """A RAM profile of a DDS channel.
 
-        All RAM profiles in the same AD9910 has access to the same 1024 x
-        32-bits RAM. There are 1024 entries, each entry occupies 32-bits.
+    RAM profile is an operation mode that feeds RAM data into the DDS as DDS
+    parameter(s) (frequency/phase/amplitude). At an user-defined time interval,
+    the DDS fetches a new RAM data and updates the selected parameter. See the
+    arguments of the constructor (__init__(..)) to tune the interval, DDS
+    parameter and data selection of the RAM playback.
+
+    All RAM profiles in the same AD9910 has access to the same 1024 x 32-bits
+    RAM. There are 1024 entries, each entry occupies 32-bits.
+
+    Unlike single-tone profiles, RAM profiles only specifies a subset of
+    parameters. The remaining parameters are specified through the FTW/POW/ASF
+    registers instead (See args base_frequency, base_phase, base_amplitude).
+    Note that unnecessary parameters will be ignored.
+
+    For example, a frequency RAM profile feeds RAM as frequencies. Phase and
+    amplitude are specified through POW and ASF registers respectively. These
+    registers are controlled by the base_phase and base_amplitude args.
+
+    Reference: AD9910 datasheet (Rev. E), Theory of Operation, Mode Priority,
+    Table 5: Data Source Priority
+
+    Note: There are different data latencies between different RAM types.
+    Amplitude takes 36 fewer sysclk cycles to reach the DDS output. Refer to
+    AD9910 datasheet (Rev. E) p.6, Data Latency.
+
+    Attributes:
+        ftw: int, the frequency tuning word for the FTW register.
+        pow: int, the phase offset word for the POW register.
+        asf: int, the amplitude scale factor for the ASF register.
+        dest: int, the RAM data destination.
+        start_addr: int, the starting RAM address of the RAM profile.
+        end_addr: int, the ending (last) RAM address of the RAM profile.
+        osk_enable: int, enable/disable OSK for the RAM profile if 1/0.
+        ram: list of int, encoded data for the RAM.
+        step: int, the number of sysclk cycles per RAM step.
+        ram_mode: int, the playback mode of the RAM.
+
+    Args:
+        dds: AD9910, the DDS that will playback the RAM profile.
+        data: list of float/2-tuples (elaborated below), the data (amplitude,
+            phase, frequencies) to be put into the RAM.
+            The type should be "list of float" for frequency/phase/amplitude
+            RAM; and list of 2-tuples for polar RAM. The tuple consists of 2
+            floats. The first represents phase, and the second represents
+            amplitude.
+        ramp_interval: float, the time interval between each step of the RAM
+            mode playback. Keep the interval at a multiple of 4*T_sysclk
+            (4*1 ns).
+        ram_type: RAMType, see the RAMType enum.
+        ram_mode: int, the playback mode of the RAM. AD9910 allows the
+            following RAM playback modes.
+
+            - RAM_MODE_DIRECTSWITCH:
+                Only the first data in the RAM profile is fed to the DDS
+
+            The other modes can fetch new RAM data to the DDS. The main
+            difference is the behavior once the last data is fetched.
+
+            - RAM_MODE_RAMPUP:
+                DDS will not fetch anything after getting the last data.
+            - RAM_MODE_BIDIR_RAMP:
+                Same as RAM_MODE_RAMPUP. However, an additional ramp down mode
+                is supported by setting profile=1.
+            - RAM_MODE_CONT_BIDIR_RAMP:
+                The RAM profile will ramp down after getting the last data.
+                Then ramp up after getting the first data, and so on.
+            - RAM_MODE_CONT_RAMPUP:
+                The RAM profile will repeat itself after getting the last data
+                from the RAM.
+
+            See the following sections on the datasheet regarding the exact
+            behavior of these 5 modes.
+            - RAM Direct Switch Mode (p.33)
+            - RAM Ramp-Up Mode (p.34, Figure 43)
+            - RAM Bidirectional Ramp Mode (p.38, Figure 46)
+            - RAM Continuous Bidirectional Ramp Mode (p.39, Figure 47)
+            - RAM Continuous Recirculate Mode (p.40, Figure 48)
+        base_frequency: float, the unmodulated DDS frequency.
+            0.0 Hz by default. This argument is optional.
+        base_phase: float, the unmodulated DDS phase.
+            0.0 turns by default. This argument is optional.
+        base_amplitude: float, the unmodulated DDS amplitude.
+            1.0 by default. This argument is optional.
+
+    Raises:
+        NotImplementedError: Unsupported RAM types found.
     """
     RAM_SIZE = 1024
 
     def __init__(self, dds, data, ramp_interval, ram_type, ram_mode,
                  base_frequency=0, base_phase=0, base_amplitude=1.0):
-        """Generate a RAM profile to a DDS channel.
-            Unlike single-tone profiles, RAM profiles only specifies a subset
-            of parameters. The remaining parameters are specified through the
-            FTW/POW/ASF registers instead (See args base_frequency, base_phase,
-            base_amplitude). Note that unnecessary parameters will be ignored.
-
-            For example, a frequency RAM profile feeds RAM as frequencies.
-            Phase and amplitude are specified through POW and ASF registers
-            respectively. These registers are controlled by the base_phase and
-            base_amplitude args.
-
-            Reference: AD9910 datasheet (Rev. E), Theory of Operation, Mode
-            Priority, Table 5: Data Source Priority
-
-            Note: There are different data latencies between different RAM
-            types. Amplitude takes 36 fewer sysclk cycles to reach the DDS
-            output. Refer to AD9910 datasheet (Rev. E) p.6, Data Latency
-
-        Args:
-            dds: AD9910, the DDS that will playback the RAM profile.
-            data: list of float/2-tuples (elaborated below), the data
-                (amplitude, phase, frequencies) to be put into the RAM.
-                The type should be "list of float" for frequency/phase/
-                amplitude RAM; and list of 2-tuples for polar RAM. The tuple
-                consists of 2 floats. The first represents phase, and the
-                second represents amplitude.
-            ramp_interval: float, the time interval between each step of the
-                RAM mode playback. Keep the interval at a multiple of
-                4*T_sysclk (4*1 ns).
-            ram_type: RAMType, see the RAMType enum.
-            ram_mode: int, the playback mode of the RAM. AD9910 allows the
-                following RAM playback modes.
-
-                - RAM_MODE_DIRECTSWITCH:
-                    Only the first data in the RAM profile is fed to the DDS
-
-                The other modes can fetch new RAM data to the DDS. The main
-                difference is the behavior once the last data is fetched.
-
-                - RAM_MODE_RAMPUP:
-                    DDS will not fetch anything after getting the last data.
-                - RAM_MODE_BIDIR_RAMP:
-                    Same as RAM_MODE_RAMPUP. However, an additional ramp down
-                    mode is supported by setting profile=1.
-                - RAM_MODE_CONT_BIDIR_RAMP:
-                    The RAM profile will ramp down after getting the last data.
-                    Then ramp up after getting the first data, and so on.
-                - RAM_MODE_CONT_RAMPUP:
-                    The RAM profile will repeat itself after getting the last
-                    data from the RAM.
-
-                See the following sections on the datasheet regarding the
-                exact behavior of these 5 modes.
-                - RAM Direct Switch Mode (p.33)
-                - RAM Ramp-Up Mode (p.34, Figure 43)
-                - RAM Bidirectional Ramp Mode (p.38, Figure 46)
-                - RAM Continuous Bidirectional Ramp Mode (p.39, Figure 47)
-                - RAM Continuous Recirculate Mode (p.40, Figure 48)
-            base_frequency: float, the unmodulated DDS frequency.
-                0.0 Hz by default. This argument is optional.
-            base_phase: float, the unmodulated DDS phase.
-                0.0 turns by default. This argument is optional.
-            base_amplitude: float, the unmodulated DDS amplitude.
-                1.0 by default. This argument is optional.
-
-        Raises:
-            NotImplementedError: Unsupported RAM types found.
-        """
         # Make sure the RAM can hold the entire sequence
         assert (len(data) <= RAMProfile.RAM_SIZE)
 
@@ -171,19 +183,18 @@ class RAMProfile:
 
 
 class RAMProfileMap:
-    def __init__(self, core):
-        """Initialize a RAM profile to DDS mapping
+    """A mapping between RAM profiles and the DDS that performs the playback.
 
-        Args:
-            core: Core, the ARTIQ Core instance for time control.
-        """
-        self.ram_profile_map = []
-        self.cplds = []
-        self.core = core
+    Args:
+        core: Core, the ARTIQ Core instance for time control.
+    """
+    def __init__(self, core):
 
     def append(self, dds, ram_profile):
-        """Append a RAM profile into the builder. In addition, register the
-            CPLDs that have DDSes that playback the RAM profile.
+        """Append a RAM profile into the builder.
+
+        In addition, register the CPLDs that have DDSes that playback the RAM
+        profile.
 
         Args:
             dds: AD9910, the DDS that will playback the RAM profile.
@@ -201,7 +212,9 @@ class RAMProfileMap:
 
     @kernel
     def load_ram(self):
-        """Load RAM operation. It initializes the RAM content and profiles.
+        """Load RAM operation.
+
+        It initializes the RAM content and profiles.
 
         Timing: (Figures generated using Kasli 2.0, 1024 RAM entries, 1 DDS)
             The entire write_ram() call is measured to have taken ~600 us
@@ -261,9 +274,9 @@ class RAMProfileMap:
     @kernel
     def enable(self):
         """Set register appropriately for enabling RAM mode.
-            After the function is called. RAM mode is NOT YET active.
-            Commit RAM enable by calling commit_enable() after.
 
+        After the function is called. RAM mode is NOT YET active.
+        Commit RAM enable by calling commit_enable() after.
         """
         # Queue in RAM enable operations to each DDS
         for dds, ram_profile in self.ram_profile_map:
@@ -275,7 +288,6 @@ class RAMProfileMap:
     def commit_enable(self):
         """Commit the previous enable() call. The RAM profile will always
             start from the designated first address.
-
         """
         # Start RAM mode using the same profile switch update.
         # This is achieved by invoking SPI transactions in the same timestamp.
@@ -289,9 +301,9 @@ class RAMProfileMap:
     @kernel
     def disable(self):
         """Set register appropriately for disabling RAM mode.
-            After the function is called. RAM mode is STILL active.
-            Commit RAM disable by calling commit_disable() after.
 
+        After the function is called. RAM mode is STILL active. Commit RAM
+        disable by calling commit_disable() after.
         """
         for dds, _ in self.ram_profile_map:
             # Disable RAM and OSK.
@@ -302,9 +314,9 @@ class RAMProfileMap:
     @kernel
     def commit_disable(self):
         """Commit the previous disable() call.
-            After this call has taken effect (in terms of the ARTIQ timeline),
-            single-tone profiles are enabled instead.
 
+        After this call has taken effect (in terms of the ARTIQ timeline),
+        single-tone profiles are enabled instead.
         """
         # End RAM mode using the same profile switch update.
         # This is achieved by invoking SPI transactions in the same timestamp.
