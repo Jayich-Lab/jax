337a58ede4d19c189d6a8ab99bbd5edc80da1d3a
jpagett <pagett.jared@gmail.com>
2023-04-28 14:20:29 -0700

    corrected docstring

--- base/experiments/ad9910_ram.py
+++ base/experiments/ad9910_ram.py
@@ -60,7 +60,8 @@ class RAMProfile:
         step: int, the number of sysclk cycles per RAM step.
         ram_mode: int, the playback mode of the RAM.
         ram_type: RAMType, the type of RAM.
-        nodwell_high: No-dwell high bit (default: 0, see AD9910 documentation)
+        nodwell_high: No-dwell high bit (default: 0, see AD9910 documentation).
+        Ignored except for in RAM Ramp-Up Mode.
 
     Args:
         dds: AD9910, the DDS that will playback the RAM profile.
@@ -102,7 +103,8 @@ class RAMProfile:
             - RAM Bidirectional Ramp Mode (p.38, Figure 46)
             - RAM Continuous Bidirectional Ramp Mode (p.39, Figure 47)
             - RAM Continuous Recirculate Mode (p.40, Figure 48)
-        dwell_end: negated no-dwell high bit (default: 1).
+        dwell_end: negated no-dwell high bit (default: 1). Ignored except for in
+        RAM Ramp-Up Mode.
 
     Raises:
         ValueError: Unsupported RAM configuration found.
